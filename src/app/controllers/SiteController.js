f35d24bc3301333670d86bf050f0504fa181f24b
KhoiDinh <khoi.dinhiamkhoi@hcmut.edu.vn>
2023-03-27 15:24:25 +0700

    commit 2

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -35,6 +35,22 @@ const fetchHumiOption = {
     },
 };
 
+const fetchLockOption = {
+    url: `https://io.adafruit.com/api/v2/ThangPham782/feeds/lock/data/last`,
+    headers: {
+        'X-AIO-Key': 'aio_Caoq909jTjFmAkCGzKqnY584zn6D',
+        'Content-Type': 'application/json',
+    },
+};
+
+const fetchDetectOption = {
+    url: `https://io.adafruit.com/api/v2/ThangPham782/feeds/alarm/data/last`,
+    headers: {
+        'X-AIO-Key': 'aio_Caoq909jTjFmAkCGzKqnY584zn6D',
+        'Content-Type': 'application/json',
+    },
+};
+
 const PostLedOption = {
     url: `https://io.adafruit.com/api/v2/ThangPham782/feeds/led/data/`,
     headers: {
@@ -51,11 +67,28 @@ const PostFanOption = {
     },
 };
 
+const PostLockOption = {
+    url: `https://io.adafruit.com/api/v2/ThangPham782/feeds/lock/data/`,
+    headers: {
+        'X-AIO-Key': 'aio_Caoq909jTjFmAkCGzKqnY584zn6D',
+        'Content-Type': 'application/json',
+    },
+};
+
+const PostAlarmOption = {
+    url: `https://io.adafruit.com/api/v2/ThangPham782/feeds/alarm/data/`,
+    headers: {
+        'X-AIO-Key': 'aio_Caoq909jTjFmAkCGzKqnY584zn6D',
+        'Content-Type': 'application/json',
+    },
+};
+
 class SiteController {
     index(req, res, next) {
         const statusData = {
             led: '',
             fan: '',
+            lock: '',
             temp: [],
             humi: [],
         };
@@ -81,6 +114,20 @@ class SiteController {
                 } else {
                     const data = JSON.parse(body);
                     statusData.fan = data.value;
+
+                    resolve();
+                }
+            });
+        });
+
+        const lockPromise = new Promise((resolve, reject) => {
+            request(fetchLockOption, (error, response, body) => {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                } else {
+                    const data = JSON.parse(body);
+                    statusData.lock = data.value;
 
                     resolve();
                 }
@@ -149,7 +196,7 @@ class SiteController {
                     }
                 },
             );
-        } else {
+        } else if (deviceType == 'fan') {
             request.post(
                 {
                     url: PostFanOption.url,
@@ -164,7 +211,50 @@ class SiteController {
                     }
                 },
             );
+        } else if (deviceType === 'lock') {
+            request.post(
+                {
+                    url: PostLockOption.url,
+                    headers: PostLockOption.headers,
+                    json: data,
+                },
+                (error, response, body) => {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        res.redirect('back');
+                    }
+                },
+            );
+        } else {
+            request.post(
+                {
+                    url: PostAlarmOption.url,
+                    headers: PostAlarmOption.headers,
+                    json: data,
+                },
+                (error, response, body) => {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        res.redirect('back');
+                    }
+                },
+            );
         }
+    }
+
+    handleDetection(req, res, next) {
+        request(fetchDetectOption, (error, response, body) => {
+            if (error) {
+                console.log('Error fetching sensor data:', error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            } else {
+                // Parse the JSON response from Adafruit and return it as a JSON object
+                const data = JSON.parse(body);
+                res.json(data);
+            }
+        });
     }
 }
 
--- src/routes/site.js
+++ src/routes/site.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const siteController = require('../app/controllers/SiteController');
 
+router.get('/detection', siteController.handleDetection);
 router.post('/toggle/:device/:status', siteController.toggle);
 router.get('/', siteController.index);
 
